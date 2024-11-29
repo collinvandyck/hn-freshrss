9ced1ba37f631cf3d5b6fbc43e1c35b19b97b580
Collin Van Dyck <collinvandyck@gmail.com>
2024-11-28 22:06:16 -0500

    try using label id

--- background.js
+++ background.js
@@ -17,7 +17,7 @@ browser.runtime.onMessage.addListener(async (message, sender) => {
                     's': `feed/${rssUrl}`,
                     't': 'TEST TEST TEST',
                     'ac': 'subscribe',
-                    'a': 'label/HN Searches',
+                    'a': 'label/8',
                 })
             };
             console.log("background.js sending request:", request);
